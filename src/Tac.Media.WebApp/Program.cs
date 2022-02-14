b1dcf8602aa8d285ed2cbe3f92d46f74ff65b676
Luis Rodrigues <luis.ms.rodrigues@hotmail.com>
2022-02-14 13:07:08 -0100

    Upgraded angular cli

--- src/Tac.Media.WebApp/Program.cs
+++ src/Tac.Media.WebApp/Program.cs
@@ -17,10 +17,10 @@ namespace Tac.Media.WebApp
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>().UseUrls("http://*:8080");
+            });
     }
 }
